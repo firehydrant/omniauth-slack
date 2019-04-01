29a80007bdeb27e69b872bfa88df60bd10cc0192
Robert Ross <robert@firehydrant.io>
2019-04-01 13:00:10 -0400

    Add callback url so oauth calls work

--- lib/omniauth/strategies/slack.rb
+++ lib/omniauth/strategies/slack.rb
@@ -106,6 +106,10 @@ module OmniAuth
         scopes = options['scope'].split(',')
         (scopes & bot_scopes).any?
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
